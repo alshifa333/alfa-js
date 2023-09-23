cabe12b7896d6608c03501a1d07f881006e61851
Alshifa <125630170+alshifa333@users.noreply.github.com>
2023-09-23 19:47:17 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all the js code as newer code means which is use in the new version
+//alert(3+3) // we are using nodejs not browser 
+//code readibility should be high
+
+//PRIMITIVE Data types in JS
+/*
+number 2-2^53
+bigint
+string=""
+undefine=> datatype in which the value of variable is not defined(abhi value assign nahi hui hai)
+null=> standalone value(value is empty)
+symbol=>unique
+*/
+
+var a=10;
+
+//LET'S CHECK TH TYPEOF
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+console.log(typeof a);
