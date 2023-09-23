088d3e43661dc27851f45ee38ff7e83c51513612
Alshifa <125630170+alshifa333@users.noreply.github.com>
2023-09-23 19:17:14 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId= 199;
+//const can never be changed
+let accountEmail="alshifathearmy78@gmail.com";
+var accountPass="121";
+accountCity="Bareilly"; //prefer not to use this
+let accountState; // if we do not input any value in the variable then it will return undefined
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPass,accountCity]);
+/*
+prefer not to use the var 
+because of issue in block scope and functional scope
+*/
