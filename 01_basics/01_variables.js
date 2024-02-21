f2edece980ebab4cec859fff45d57f915d3917ae
Alshifa <125630170+alshifa333@users.noreply.github.com>
2024-02-21 12:08:18 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,7 +2,7 @@ const accountId= 199;
 //const can never be changed
 let accountEmail="alshifathearmy78@gmail.com";
 var accountPass="121";
-accountCity="Bareilly"; //prefer not to use this
+accountCity="Bareilly"; //prefer not to use this wihtout using let var 
 let accountState; // if we do not input any value in the variable then it will return undefined
 
 console.log(accountId);
